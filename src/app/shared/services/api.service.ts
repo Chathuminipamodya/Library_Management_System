26ff9197ccc78e73f0f5a9426038b0b2a9c53ad8
Chathuminipamodya <138112305+Chathuminipamodya@users.noreply.github.com>
2026-01-16 23:41:25 +0530

    new changes

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,15 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   baseUrl: string = "https://localhost:7286/api/Library/"
+  userStatus: Subject<string> = new Subject();
   constructor(private http:HttpClient) { }
+
   register(user:any){
      return this.http.post(this.baseUrl + 'Register',user,{
       responseType: 'text',
     })
+  }
+   login(info: any) {
+    let params = new HttpParams()
+      .append('email', info.email)
+      .append('password', info.password);
+
+    return this.http.get(this.baseUrl + 'Login', {
+      params: params,
+      responseType: 'text',
+    });
   }
 }
